57d8524950b367ccf96b15edc4b46de1f4ce85bc
satyaki123 <satyaki.nmiet@gmail.com>
2014-06-20 00:06:09 +0530

    added assets precompile

--- config/routes.rb
+++ config/routes.rb
@@ -52,7 +52,6 @@ BookInventory::Application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   root :to => 'books#index'
